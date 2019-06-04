0ea6e1618df7b2911e24265823f85d8ccf60865e
Hiroyuki Tanjo <expheno@gmail.com>
2019-06-04 23:53:57 +0900

    Install webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './public/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
